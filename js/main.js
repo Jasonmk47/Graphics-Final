2382a85e8dbc3ba6e19228dd3fa37d8579b90c89
savdu <savannah.du@gmail.com>
2017-05-14 14:32:52 -0400

    setup

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
 
-function canvasSand() {
+// function canvasSand() {
 
    // Get the canvas element
-   var canvas = document.getElementById('canvas')
+   var canvas = document.getElementById('canvas');
+   canvas.width = document.body.clientWidth;
+   canvas.height = document.body.clientHeight;
 
-   // If canvas element exists:
-   if (canvas.getContext) { 
+   // Specify 2d canvas type
+   var ctx = canvas.getContext('2d');
 
-      // Specify 2d canvas type
-      ctx = getContext('2d');
+   var centerX = canvas.width / 2;
+   var centerY = canvas.height / 2;
 
-      // Draw the cup
-      cup();
+   // Draw the cup
+   cup();
 
-      // Draw the sand particles
-      sand();
+   // Draw the sand particles
+   sand();
 
-   }
-}
+// }
 
 function cup() {
 
    // Draw the cup
-   
+   ctx.strokeStyle = '#555555';
+   // ctx.fillRect(centerX-50, centerY, 100, 100);
+   // ctx.clearRect(centerX-50, centerY, 100, 100);
+   ctx.strokeRect(centerX-250, centerY, 500, 100);
+}
+
+function sand() {
 
 }
